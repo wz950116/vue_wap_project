c1f1a597a58303beb2160804b9d162eae203144b
wz950116 <1328380414@qq.com>
2018-06-07 17:39:53 +0800

    Update request.js

--- demo1/src/model/request.js
+++ demo1/src/model/request.js
@@ -1,8 +1,23 @@
 import Axios from 'axios'
 import Vue from 'vue'
+import Cookies from 'js-cookie'
 
 let mock = false // true
 let baseURL = '//productivity.focus' +  window.env + '.cn/api';
+
+// request拦截器
+// service.interceptors.request.use(config => {
+//   if (store.getters.token || Cookies.get('TokenKey')) {
+//     // 请求头携带含有token的cookie
+//     config.headers.Authorization = 'bearer ' + (getToken() ? getToken() : store.getters.token)
+//   }
+//   config.headers['lang'] = Cookies.get('language') || 'zh'
+//   return config
+// }, error => {
+//   // Do something with request error
+//   console.log(error) // for debug
+//   Promise.reject(error)
+// })
 
 // response拦截器
 Axios.interceptors.response.use(response => {
